d31e2648ec9cf820d96d9d79e8813d87825e41f5
lzxb <1340641314@qq.com>
2015-11-12 23:04:32 +0800

    更新到2.0版本，优化加载机制

--- js/SeeDownloadImg.js
+++ js/SeeDownloadImg.js
@@ -4,90 +4,92 @@
 		QQ: 1340641314,
 		github: 'https://github.com/1340641314/SeeDownloadImg',
 		statement: '在保留头部版权的情况下，可以自由发布修改，应用于商业用途',
-		version: '1.0.0',
-		update: '2015-10-23'
+		version: '2.0.0',
+		update: '2015-11-12'
 	};
 */
-
 (function () {
 	
-	var SeeDownloadImg = function (query, config) {
-		this.config = {
-			top: 10, //元素在顶部伸出的高度
-			bottom: 10, //元素在底部伸出的高度
-			updateTag: false, //循环结束后，是否自动更新标签，true为更新，false不更新
-			fnLoadImg: function () {} //真实图片加载成功后执行
-		};
-		console.log(query);
-		this.query = query? query : '[data-lz-src]'; //选择器
-		console.log(this.query);
-		this.tag = null; //存储标签
+	function SeeDownloadImg (setting){
 		
-		for (attr in config) { //更新配置
-			this.config[attr] = config[attr];
-		}
+		this.selector = '[data-lz-src]'; //选择器
+		this.iTop = setting.iTop || 0;         //元素在顶部伸出的距离
+		this.iBottom = setting.iBottom || 0;      //元素在底部伸出的距离
+		this.fLoadImg = setting.fLoadImg || function (oImg){}; //图片加载完成后执行方法
+		this.aElements = [];   //存储元素
+		this.status = true;    //检测状态值
 		
 		this.init();
-	};
-	
-	SeeDownloadImg.prototype = {
-		init: function () {
-			var _this = this;
+	}
+	SeeDownloadImg.prototype.init = function () {
+		var _this = this;
+		if (!this.initStatus) window.addEventListener('load',function () {
 			
-			window.addEventListener('load', function () {
-				_this.getTag();
-				_this.each();
-			}, false);
+			_this.getElements(); //获取元素
+			_this.each();
+		}, false);
+		
+		window.addEventListener('scroll', function () {
 			
-			window.addEventListener('scroll', function () {
+			if (_this.status) {
 				_this.each();
-				if (_this.config.updateTag) { //更新标签
-					_this.getTag();
-				}
-				
-			}, false);
-		},
-		getTag: function () { //更新标签
-			this.tag = document.querySelectorAll(this.query);
-			this.tag = Array.prototype.slice.call(this.tag);
-		},
-		each: function () { //循环标签
+			} else {
+				_this.getElements();
+			}
+		}, false);
+	}
+	SeeDownloadImg.prototype.getElements = function () {
+		
+		var aTag = document.querySelectorAll(this.selector);
+		
+		for (var i=0;i<aTag.length;i++) {
+            var json ={};
+            json.tag = aTag[i];
+            json.src = aTag[i].dataset.lzSrc;
+            delete aTag[i].dataset.lzSrc;
+            
+            this.aElements.push(json);
+		}
+	}
+    SeeDownloadImg.prototype.each = function () {
+        this.status = false;
+        var aElements = this.aElements;
 
-			for (var i=0;i<this.tag.length;i++) {
-				
-				if (this.tsetTap(this.tag[i])) {
+        for (var i=0;i<aElements.length;i++) {
 
-					this.loadImg(this.tag[i]);
-					delete this.tag[i].dataset.lzSrc;
-					this.tag.splice(i,1);
-					i--;
-				}
-				
-			};
-		},
-		tsetTap: function (obj) { //判断元素是否加载条件
+            if (this.testMeet(aElements[i].tag)) {
+				this.loadImg(aElements[i].tag, aElements[i].src);
+				aElements.splice(i,1);
+				i--;
+            } 
+            
+        }
+		this.status = true; //做缓处理，避免因为用户滑动过快导致的内存上涨
+    }
+    SeeDownloadImg.prototype.testMeet = function (obj) {
+
 			var iObjHeight = obj.offsetHeight; //元素的高度
 			var iWinHeight = document.documentElement.clientHeight; //可视区的高度
 			
 			var iDistanceTop = (obj.getBoundingClientRect().top + iObjHeight); //元素在顶部伸出的高度
 			var iDistanceBottom = ((iWinHeight + iObjHeight) - obj.getBoundingClientRect().bottom); //元素在底部伸出的高度
 
-			var bool = iDistanceTop >= this.config.top && iDistanceBottom >= this.config.bottom;
+			var bool = iDistanceTop >= this.iTop && iDistanceBottom >= this.iBottom;
+
 			return bool;
-	
-		},
-		loadImg: function (oImg) { //将图片渲染为真实的地址
+    }
+	SeeDownloadImg.prototype.loadImg = function (oImg, url) {
 		
 			var _this = this;
 			var oImgObj = new Image();
-			oImgObj.src = oImg.dataset.lzSrc;
+			oImgObj.src = url;
 
 			oImgObj.addEventListener('load', function () {
 				oImg.src = oImgObj.src;
-				_this.config.fnLoadImg(oImg);
+				_this.fLoadImg(oImg);
 			},false);
-			
-		}
-	};
+	}
+	
 	window.SeeDownloadImg = SeeDownloadImg;
 })();
+
