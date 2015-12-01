65b20981f4465f834f6bd6f7fe42ceeb010883af
狼族小狈 <1340641314@qq.com>
2015-12-01 11:54:13 +0800

    网页可见区域高修复

--- js/SeeDownloadImg.js
+++ js/SeeDownloadImg.js
@@ -89,7 +89,7 @@
 
     var status = element.getBoundingClientRect(); //取得元素在屏幕的位置信息
     var iObjHeight = element.offsetHeight; //元素自身的高度
-    var iWinHeight = window.screen.availHeight; //工作可用的高度
+    var iWinHeight = document.body.clientHeight; //网页可见区域高
     var conut = -1;
 
     for (attr in status) {
